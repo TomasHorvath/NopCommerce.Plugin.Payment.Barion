36e40593ba28515530c3f0811ba867e3e42ce3fd
Tomáš Horváth <thsoftware@gmail.com>
2019-07-22 18:31:06 +0200

    init repo

---
+++ Nop.Plugin.Misc.Barion/Events/TransactionApprovedEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Plugin.Misc.Barion.Events
+{
+  
+    /// <summary>
+    /// Barion payment approved event
+    /// </summary>
+    public class TransactionApprovedEvent
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="blogComment">BarionTransactionDTO</param>
+        public TransactionApprovedEvent(Model.BarionTransactionDTO transactionInfo)
+        {
+            TransactionInfo = transactionInfo;
+        }
+
+        /// <summary>
+        /// Payment transaction data
+        /// </summary>
+        public Model.BarionTransactionDTO TransactionInfo { get; }
+    }
+}
---
+++ Nop.Plugin.Misc.Barion/Model/BarionTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Core;
+
+namespace Nop.Plugin.Misc.Barion.Model
+{
+    public class BarionTransactionDTO 
+    {
+        public int OrderId { get; set; }
+        public string PaymentId { get; set; }
+        public int PaymentStatus { get; set; }
+        public decimal OrderTotal { get; set; }
+
+    }
+}
